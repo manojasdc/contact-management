45717473ed7d8b579cd6da4481d35022e646f98d
manoj <manojrajoriya1995gmail.com>
2025-05-07 16:36:01 +0530

    sout added

--- src/main/java/com/form/FormLoginApplication.java
+++ src/main/java/com/form/FormLoginApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FormLoginApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting FormLoginApplication...");
 		SpringApplication.run(FormLoginApplication.class, args);
 	}
 
